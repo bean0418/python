7cf6b2de0e3df9bbaf9c7fcd267657112bb73630
bean0418 <imb2an@gmail.com>
2021-06-13 21:49:23 +0900

    edited

--- infor2.py
+++ infor2.py
@@ -4,9 +4,9 @@ school_grades = 0 # 등급 * 단위
 result = 0 # 최종 내신
 
 while True:
-    subj = input("[내신 산출 계산기]\n* 중지하시려면 중지를 입력해주세요\n* 소수점은 둘째자리까지 반올림되어 표기됩니다.\n과목 이름 입력: ")
+    subj = input("[내신 산출 계산기]\n* 과목 입력을 중지하시려면 중지를 입력해주세요\n* 소수점은 둘째자리까지 반올림되어 표기됩니다.\n과목 이름 입력: ")
     
-    if subj == "중지":
+    if subj == "중지" and len(subj_list) != 0:
         print("[과목 입력을 중지]")
         print("입력된 과목의 수는", str(len(subj_list)) + "개입니다.")
         subj_list.append("과목없음") # 리스트의 마지막
@@ -20,6 +20,10 @@ while True:
             school_grades += float(grade) * float(unit)
             unit_a += float(unit) # 총 단위 더하기
         break
+    elif subj == "중지" and len(subj_list) == 0:
+        print("입력된 과목이 없습니다.")
+        break
+
     else:
         subj_list.append(subj) # 중지가 아니면 과목 계속 추가
         continue
